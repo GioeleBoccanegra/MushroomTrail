9575d12fbb28f66e9a05a332cd5265d7f881652a
giobocca03 <gio.bocca03@gmail.com>
2025-08-04 13:47:05 +0200

    cerato rpimo scheletro frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css"
+import React from 'react'
+
+
+export default function Navbar() {
+  return (
+    <nav>
+      <a>p</a>
+    </nav>
+  )
+}
---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <p>shduisgfuiby</p>
+    </div>
+  )
+}
