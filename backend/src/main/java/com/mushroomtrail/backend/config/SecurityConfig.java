b3c546c6cfaa808978d005f4f8ea637f8168719b
giobocca03 <gio.bocca03@gmail.com>
2025-08-05 17:04:56 +0200

    aggiunto cross origin per frontend

--- backend/src/main/java/com/mushroomtrail/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/mushroomtrail/backend/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.mushroomtrail.backend.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,6 +9,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 // Questo package contiene le classi di configurazione dell'applicazione
 // Questa annotazione dice a Spring che questa classe contiene dei bean da gestire
@@ -32,6 +37,26 @@ public class SecurityConfig {
         .formLogin(form -> form.disable())
         .httpBasic(httpBasic -> httpBasic.disable());
     return http.build();
+  }
+
+  // cross origin
+  @Bean
+  public CorsConfigurationSource CorsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of(
+        "http://localhost:5173"));
+
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+    configuration.setAllowedHeaders(List.of("*"));
+    // Permetti l'invio di cookie e credenziali (utile se li usi)
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+
   }
 
 }
