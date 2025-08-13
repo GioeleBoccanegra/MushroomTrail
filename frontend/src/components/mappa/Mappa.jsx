ea411d6676b06696791be24eddb28e84123434e9
giobocca03 <gio.bocca03@gmail.com>
2025-08-13 15:22:26 +0200

    modfica dimensione segnalatore mappa

--- frontend/src/components/mappa/Mappa.jsx
+++ frontend/src/components/mappa/Mappa.jsx
@@ -19,8 +19,8 @@ const redIcon = new L.Icon({
 
 const greenIcon = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
+  iconSize: [6, 20.5],
+  iconAnchor: [6, 20.5],
   popupAnchor: [1, -34],
   shadowUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png',
   shadowSize: [41, 41]
